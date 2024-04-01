4f724a30b6e50e6885011c6d886ed651dee853e7
oscar <oscnet@163.com>
2024-04-01 13:27:59 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = [
+    'PyYAML',
+    'aiofiles',
+    'aiohttp'
+]
+
+setup(
+    name='trade-lib',
+    description="我的交易库",
+    version='0.1.3',
+    py_modules=['trade_lib'],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=install_requires,
+)
